c4f9a207dfcbb677668ebc4b5dce1aa89661dfc3
zihao <1819851507@qq.com>
2020-09-26 15:36:18 +0800

    index第一个页面

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,23 +9,36 @@
 				<input type="text" value="" />
 			</view>
 		</view>
-		
+
 		<!-- 头部菜单 -->
 		<view class="topmenu">
-			<scroll-view class="wz" scroll-x="true" style="padding-right: 50rpx;">
-				<text v-for="(item,index) in topdata" :key="index" :class="index == topmenuselect?'select wzbox':'<wzbox></wzbox>'" >
+			<scroll-view class="wz" scroll-x="true" style="padding-right: 50rpx;" v-if="!show">
+				<text v-for="(item,index) in topdata" 
+				:key="index" 
+				:class="index == topmenuselect?'select wzbox':'wzbox'"
+				 @click="select(index)">
 					{{item.name}}
 				</text>
 			</scroll-view>
-			<view class=" jt" @click="display">
+			<view class="jt" @click="display">
 				<view class="xz">
 					<image class="down1" src="../../static/images/down.png" :class="show==true?'xz180':'z180'" style="width: 30rpx;height: 30rpx;"></image>
+				</view>
+			</view>
+			<view :class="show==true?'expansion hs':'expansion'">
+				<view class="alltelep">
+					全部
+				</view>
+				<view class="nr">
+					<view class="nrbox" v-for="(item,index) in topdata" :key="index" @click="select(index)">
+						<view :class="index == topmenuselect?'nrselect nrys':'nrys' " @click="display">{{item.name}}</view>
+					</view>
 				</view>
 			</view>
 		</view>
 
 		<!-- 轮播图 -->
-		<scroll-view class="lbt">
+		<scroll-view :class="show?'lbt zhangw':'lbt'" >
 			<swiper indicator-dots="true" indicator-active-color="#fff" autoplay="true">
 				<swiper-item v-for="item in lbtdata" :style="{height:item.h +'rpx'}">
 					<image :src="item.img_url" style="width: 100%;height: 100%;"></image>
@@ -39,14 +52,14 @@
 				<image :src="item.img_url" style="width: 100%;height: 100%;"></image>
 			</uni-grid-item>
 		</uni-grid>
-		
+
 		<!-- 三张图片 -->
 		<view class="multi_cell">
 			<view class="one">
 				<image :src="multiCellData[0].img_url" :style="{height: multiCellData[0].h+'rpx'}"></image>
 			</view>
 			<view class="two">
-				<view class="shohuan" >
+				<view class="shohuan">
 					<image :src="multiCellData[1].img_url" :style="{height: multiCellData[1].h+'rpx'}"></image>
 				</view>
 				<view class="dianshi">
@@ -54,13 +67,13 @@
 				</view>
 			</view>
 		</view>
-		
+
 		<!-- 小米手机到米家家电 -->
 		<view class="moban" v-for="(items,index1) in moban" :key="index1">
 			<view class="mobantu" v-for="(imgitem,index) in items[0].body.items" :key="imgindex">
-				<image :src="imgitem.img_url" :style="{width: '100%', height: items[0].body.items[0].h +'rpx'}" ></image>
+				<image :src="imgitem.img_url" :style="{width: '100%', height: items[0].body.items[0].h +'rpx'}"></image>
 			</view>
-			
+
 			<uni-grid :column="2" :showBorder='false'>
 				<uni-grid-item v-for="(item,index) in items[1].body.items" :key="index" style="height: 500rpx;margin-top:4rpx ;">
 					<view class="mobanbox">
@@ -70,12 +83,12 @@
 						<view class="msbox">
 							<view class="jj">
 								{{item.product_name}}
-							</view> 
+							</view>
 							<view class="xx">
 								{{item.product_brief}}
 							</view>
 							<view class="jg">
-								<text class="xj">￥{{item.product_price}}起  </text>
+								<text class="xj">￥{{item.product_price}}起 </text>
 								<text class="ls" v-if="!(item.product_price == item.product_org_price)">￥{{item.product_org_price}}</text>
 							</view>
 							<button size="mini" type="warn">立即购买</button>
@@ -87,14 +100,14 @@
 				{{items[2].body.items[0].action_title}}
 			</view>
 		</view>
-	
+
 		<!-- 小米智能 -->
 		<view class="zn" v-for="(items,index1) in zn" :key="index1">
 			<view class="mobantu" v-for="(imgitem,index) in items[0].body.items" :key="imgindex">
-				<image :src="imgitem.img_url" :style="{width: '100%', height: items[0].body.items[0].h +'rpx'}" ></image>
+				<image :src="imgitem.img_url" :style="{width: '100%', height: items[0].body.items[0].h +'rpx'}"></image>
 			</view>
 			<view class="zj">
-				<view class="zyxj" v-for="(item,index) in items[1].body.items" :key="index" v-if="(index%2==0)" :style="{order:index} " >
+				<view class="zyxj" v-for="(item,index) in items[1].body.items" :key="index" v-if="(index%2==0)" :style="{order:index} ">
 					<view class="z" style="height: 374rpx">
 						<image :src="item.img_url" mode=""></image>
 					</view>
@@ -103,18 +116,18 @@
 							<view class="jj">
 								{{item.product_name}}
 							</view>
-							
+
 							<view class="xx">
 								{{item.product_brief}}
 							</view>
-							
+
 							<view class="jg">
-								<text class="xj">￥{{item.product_price}}  </text>
+								<text class="xj">￥{{item.product_price}} </text>
 								<text class="ls" v-if="!(item.product_price == item.product_org_price)">￥{{item.product_org_price}}</text>
 							</view>
 						</view>
-						
-						
+
+
 					</view>
 				</view>
 				<view class="zyxj" v-for="(item,index) in items[1].body.items" :key="index" v-if="!(index%2==0)" :style="{order:index} ">
@@ -123,29 +136,29 @@
 							<view class="jj">
 								{{item.product_name}}
 							</view>
-							
+
 							<view class="xx">
 								{{item.product_brief}}
 							</view>
-							
+
 							<view class="jg">
-								<text class="xj">￥{{item.product_price}}  </text>
+								<text class="xj">￥{{item.product_price}} </text>
 								<text class="ls" v-if="!(item.product_price == item.product_org_price)">￥{{item.product_org_price}}</text>
 							</view>
 						</view>
-						
+
 					</view>
-					<view class="z"  style="height: 374rpx">
+					<view class="z" style="height: 374rpx">
 						<image :src="item.img_url" mode=""></image>
 					</view>
 				</view>
 			</view>
-			
+
 			<view class="gengduo">
 				{{items[2].body.items[0].action_title}}
 			</view>
 		</view>
-	
+
 		<!-- 了解小米 -->
 		<view class="ljxm">
 			<uni-grid :column="2" :showBorder='false'>
@@ -153,22 +166,22 @@
 					<view class="img" :class="(index%2==0)?'m':''">
 						<image :src="item.img_url" style="width: 100%;"></image>
 					</view>
-					
+
 				</uni-grid-item>
 			</uni-grid>
-			
+
 			<view class="xm" v-for="(imgitem,index) in ljxm[1].body.items" :key="imgindex">
-				<image :src="imgitem.img_url" :style="{width: '100%', height: imgitem.h +'rpx'}" ></image>
+				<image :src="imgitem.img_url" :style="{width: '100%', height: imgitem.h +'rpx'}"></image>
 			</view>
 		</view>
-			
+
 	</view>
 </template>
 <script>
 	import {
 		getTopMenuData
-	} from "../../api/indexapi.js" 
-	
+	} from "../../api/indexapi.js"
+
 	export default {
 		data() {
 			return {
@@ -176,10 +189,10 @@
 				topmenuselect: 0,
 				lbtdata: [],
 				gogei: [],
-				multiCellData:[],
-				moban:[],
-				zn:[],
-				ljxm:[],
+				multiCellData: [],
+				moban: [],
+				zn: [],
+				ljxm: [],
 				show: false,
 			}
 		},
@@ -194,7 +207,7 @@
 				data.sections[2].body.items.map((v) => {
 					temp.push(v);
 				})
-				
+
 				this.topdata = tabs;
 				this.lbtdata = data.sections[0].body.items;
 				this.gogei = temp;
@@ -202,14 +215,15 @@
 				this.moban = data.moban;
 				this.zn = data.zn;
 				this.ljxm = data.ljxm;
-				console.log(data.ljxm)
-				
-				// console.log(this.moban)
-				
+
+
 			},
-			display(){
-						// this.$refs.popup.open();
-						this.show = !this.show
+			display() {
+				// this.$refs.popup.open();
+				this.show = !this.show
+			},
+			select(index) {
+				this.topmenuselect = index;
 			}
 		},
 		created() {
@@ -219,6 +233,8 @@
 </script>
 <style lang="scss" scoped>
 	.container {
+		background-color: #F5F5F5;
+
 		.top {
 			position: sticky;
 			top: 0px;
@@ -227,6 +243,7 @@
 			height: 80rpx;
 			z-index: 100;
 			align-items: center;
+
 			.logo {
 				width: 60rpx;
 				height: 55rpx;
@@ -250,7 +267,6 @@
 		}
 
 		.topmenu {
-			position: relative;
 			padding-top: 10rpx;
 			background-color: #f2f2f2;
 			font-size: 30rpx;
@@ -258,6 +274,7 @@
 			z-index: 100;
 			position: sticky;
 			top: 80rpx;
+
 			.wz {
 				white-space: nowrap; // 滚动必须加的属性
 
@@ -265,6 +282,12 @@
 					margin: 0rpx 26rpx;
 					padding-bottom: 4rpx;
 				}
+				.wzbox:last-child{
+					margin-right: 70rpx;
+				}
+			}
+			.zbm{
+				margin-right: 30rpx;
 			}
 
 			.select {
@@ -283,16 +306,63 @@
 				position: absolute;
 				top: 0;
 				right: 0;
-				.xz{
-					.xz180{
-					  transition: all 1s;
-					  transform: rotate(180deg);
+				z-index: 100;
+
+				.xz {
+					.xz180 {
+						transition: all 1s;
+						transform: rotate(180deg);
 					}
-					.z180{
-					  transition: all 1s;
-					  transform: rotate(0deg);
+
+					.z180 {
+						transition: all 1s;
+						transform: rotate(0deg);
 					}
 				}
+			}
+
+				
+			
+			.expansion {
+				position: absolute;
+				background-color: #f2f2f2;
+				height: 0px;
+				overflow: hidden;
+				.alltelep {
+					margin: 20rpx 20rpx;
+				}
+
+				.nr {
+					display: flex;
+					flex-wrap: wrap;
+
+					.nrbox {
+						margin: 0rpx 0 25rpx 20rpx;
+						font-size: 28rpx;
+
+						.nrys {
+							display: flex;
+							align-items: center;
+							justify-content: center;
+							width: 152rpx;
+							height: 52rpx;
+							border: 2rpx solid #ccc;
+							border-radius: 6rpx;
+						}
+
+						.nrselect {
+							background-color: #fde0d5;
+							border-color: #ff6700;
+							color: #ff6700;
+						}
+					}
+
+				}
+			}
+
+			.hs{
+				transition: all 0.5s;
+				height: 250rpx;
 			}
 		}
 
@@ -300,129 +370,147 @@
 			width: 100%;
 			background-color: #ccc;
 		}
-		
-		.multi_cell{
+		.zhangw{
+			margin-top: 48rpx;
+		}
+
+		.multi_cell {
 			display: flex;
-			margin:10rpx 0rpx ;
-			.one{
+			margin: 10rpx 0rpx;
+
+			.one {
 				flex: 1;
 				margin-right: 10rpx;
 			}
-			
-			.two{
+
+			.two {
 				flex: 1;
 				display: flex;
 				flex-wrap: wrap;
-				.shohuan{
+
+				.shohuan {
 					width: 100%;
 					margin-bottom: 2rpx;
 				}
-				.dianshi{
+
+				.dianshi {
 					width: 100%;
 				}
 			}
-			image{
+
+			image {
 				width: 100%;
 			}
 		}
-		
-		.moban{
+
+		.moban {
 			background-color: #fff;
-			.mobantu{
+
+			.mobantu {
 				margin-top: 10rpx;
 			}
-			.mobanbox{
+
+			.mobanbox {
 				background-color: #FFF;
 				margin: 0 4rpx;
+
 				.mobanimg {
 					height: 270rpx;
 				}
-				.msbox{
+
+				.msbox {
 					display: flex;
-					flex-direction:column;
+					flex-direction: column;
 					align-items: center;
 					padding: 20rpx;
-					
-					
+
 				}
-				
-				
+
 			}
-			
-			
+
+
 		}
-		
-		.zn{
-			.zj{
+
+		.zn {
+			.zj {
 				display: flex;
 				flex-direction: column;
-				.zyxj{
+
+				.zyxj {
 					display: flex;
-					.z{
+
+					.z {
 						flex: 1;
-						background-color: #ccc;
-						// border: 1rpx solid #f2f2f2;  
 					}
-					.r{
+
+					.r {
+						background-color: #fff;
 						flex: 1;
-						.p{
+
+						.p {
 							padding: 60rpx;
-							.jg{
-								padding-top: 20rpx;	
+
+							.jg {
+								padding-top: 20rpx;
 							}
 						}
 					}
-					image{
+
+					image {
 						width: 100%;
 						height: 100%;
 					}
 				}
-				
+
 			}
-		} 
-		
-		.jj{
+		}
+
+		.jj {
 			font-size: 28rpx;
 			display: -webkit-box;
 			-webkit-box-orient: vertical;
 			-webkit-line-clamp: 1;
 			overflow: hidden;
 		}
-		.xx{
-			margin-top:4rpx ;
+
+		.xx {
+			margin-top: 4rpx;
 			font-size: 22rpx;
-			color: rgba(0,0,0,.54);
+			color: rgba(0, 0, 0, .56);
 		}
-		.jg{
-			margin-bottom:20rpx ;
-			.xj{
+
+		.jg {
+			margin-bottom: 20rpx;
+
+			.xj {
 				font-size: 28rpx;
 				color: #EA625B;
 				margin-right: 10rpx;
 			}
-			.ls{
+
+			.ls {
 				font-size: 22rpx;
 				color: #757575;
-				text-decoration:line-through;
+				text-decoration: line-through;
 			}
 		}
-		.gengduo{
+
+		.gengduo {
 			border-top: 1px solid #ccc;
 			display: flex;
 			justify-content: center;
-			color: #ccc;
+			color: rgba(0, 0, 0, .6);
 			padding: 36rpx;
 		}
-		
-		.ljxm{
-			.m{
+
+		.ljxm {
+			.m {
 				margin-right: 10rpx;
 			}
-			.xm{
-				margin-bottom: 10rpx ;
+
+			.xm {
+				margin-bottom: 10rpx;
 			}
 		}
-				
-				
 	}
 </style>
